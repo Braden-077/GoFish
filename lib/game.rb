4230ae650ef59db2f4080a8158b5626de06fbc0e
Braden Rich <bradenwrich@gmail.com>
2022-06-15 12:36:34 -0400

    Implement over? method for game

--- lib/game.rb
+++ lib/game.rb
@@ -3,7 +3,7 @@
 require_relative 'deck'
 
 class Game
-  attr_accessor :deck, :players, :started, :round, :round_output
+  attr_accessor :deck, :players, :started, :round, :round_output, :fish
   STARTING_HAND = 7
   def initialize(players = [], deck = Deck.new, started = false)
     @deck = deck
@@ -11,7 +11,6 @@ class Game
     @players = players
     @started = started
     @round = 1
-    deck.shuffle!
   end
 
   def start
@@ -19,12 +18,13 @@ class Game
     @started = true 
   end
 
-  def play_round(rank, asking_player, player_asked)
-    if asking_player.has_card?(rank) && player_asked.has_card?(rank)
-      outcome_one(rank, asking_player, player_asked)
+  def play_round(rank, player_asked)
+    if turn_player.has_card?(rank) && player_asked.has_card?(rank)
+      outcome_one(rank, player_asked)
     elsif !player_asked.has_card?(rank)
-      outcome_two(rank, asking_player, player_asked)
-      up_round
+      outcome_two(rank, player_asked)
+    elsif !turn_player.has_card?(rank)
+      outcome_three(rank, turn_player)
     end
   end
 
@@ -37,9 +37,10 @@ class Game
 
   def go_fish(player)
     if deck.cards_left == 0
+      up_round
       'The deck is empty.'
     else
-      player.take(deck.deal)
+      @fish = player.take(deck.deal).first.rank
     end
   end
 
@@ -48,7 +49,7 @@ class Game
   end
 
   def over?
-    false
+    players.sum {|player| player.books.length} == 13
   end
 
   def get_player(name)
@@ -57,13 +58,24 @@ class Game
 
   private 
   
-  def outcome_one(rank, asking_player, player_asked)
-    @round_output = "#{player_asked.name} had #{rank}. #{asking_player.name} go again!"
-    asking_player.take(player_asked.give(rank))
+  def outcome_one(rank, player_asked)
+    @round_output = "#{player_asked.name} had #{rank}'s. #{turn_player.name} go again!"
+    turn_player.take(player_asked.give(rank))
+    turn_player.check_for_book(rank)
   end
 
-  def outcome_two(rank, asking_player, player_asked)
+  def outcome_two(rank, player_asked)
     @round_output = "#{player_asked.name} doesn't have any #{rank}'s. Go Fish."
-    go_fish(asking_player)
+    go_fish(turn_player)
+    if @fish == rank
+      turn_player.check_for_book(rank)
+    else
+      turn_player.check_for_book(@fish)
+    up_round
+    end
+  end
+
+  def outcome_three(rank, turn_player)
+    @round_output = "Silly #{turn_player.name}, you can't ask for #{rank}! You don't have any. Try again!"
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true 
 
 require 'game'
+require 'player'
+require 'card'
 
 describe Game do
   describe '#initialize' do
@@ -14,32 +16,37 @@ describe Game do
     it 'asking player gets cards if asked player has them' do
       asked_player = Player.new('Braden', [Card.new('A', 'S'), Card.new('A', 'C'), Card.new('A', 'D')])
       asking_player = Player.new('Josh', [Card.new('A', 'H')]) 
-      game = Game.new([asking_player, asked_player])
+      game = Game.new([asking_player, asked_player], Deck.new([]), true)
       
-      game.play_round('A', asking_player, asked_player)
+      game.play_round('A', asked_player)
 
-      expect(asked_player.hand).to eq []
-      expect(asking_player.hand).to eq [Card.new('A', 'H'), Card.new('A', 'S'), Card.new('A', 'C'), Card.new('A', 'D')]
+      expect(asked_player.hand).to be_empty
+      expect(asking_player.hand).to be_empty
+      expect(asking_player.books.count).to eq 1
+      expect(asking_player.books).to eq ['A']
+      expect(game.turn_player).to eq asking_player
     end
 
     it 'makes a player go fish if the opponent does not have the specified card' do
       asked_player = Player.new('Braden', [Card.new('A', 'S'), Card.new('A', 'C'), Card.new('A', 'D')])
       asking_player = Player.new('Josh', [Card.new('A', 'H')]) 
-      game = Game.new([asking_player, asked_player])
+      game = Game.new([asking_player, asked_player], Deck.new([Card.new('K', 'C')]), true)
       
-      game.play_round('Q', asking_player, asked_player)
+      game.play_round('Q', asked_player)
 
-      expect(asking_player.hand.count).to eq 2
+      expect(asked_player.hand.count).to eq 3
       expect(asked_player.hand).to eq [Card.new('A', 'S'), Card.new('A', 'C'), Card.new('A', 'D')]
+      expect(asking_player.hand.count).to eq 2
+      expect(asking_player.hand).to match_array [Card.new('A', 'H'), Card.new('K', 'C')]
     end
   end
 
   describe '#go_fish' do
-    it 'allows the player to go fish and take a card from the deck' do
+    it 'allows the player to go fish and take a card from the deck'do
       player1 = Player.new('Braden')
       player2 = Player.new('Josh')
-      game = Game.new([player1, player2])
-      game.go_fish(player1)
+      game = Game.new([player1, player2], Deck.new([Card.new('3', 'D')]))
+      expect(game.go_fish(game.players.first)).to eq '3'
       expect(player1.card_count).to eq 1
     end
 
@@ -79,12 +86,27 @@ describe Game do
       game = Game.new([asking_player, asked_player], Deck.new, true)
       
       expect(game.turn_player.name).to eq 'Josh'
-      game.play_round('Q', asking_player, asked_player)
+      game.play_round('Q', asked_player)
       expect(game.round).to eq 2
       expect(game.turn_player.name).to eq 'Braden'
-      game.play_round('Q', asking_player, asked_player)
+      game.play_round('Q', asked_player)
       expect(game.turn_player.name).to eq 'Josh'
     end
+  end
 
+  describe '#over?' do
+    it 'returns true when all 13 books have been collected' do
+      game = Game.new([Player.new('Josh'), Player.new('Braden')])
+      game.players.first.books = ['2', '3', '4', '5', '6', '7', '8', '9', '10']
+      game.players.last.books = ['J', 'Q', 'K', 'A']
+      expect(game.over?).to be true
+    end
+
+    it 'returns true when all 13 books have been collected' do
+      game = Game.new([Player.new('Josh'), Player.new('Braden')])
+      game.players.first.books = ['2', '3', '4', '5', '6', '7', '8', '9', '10']
+      game.players.last.books = ['J', 'Q', 'K']
+      expect(game.over?).to be false
+    end    
   end
 end
